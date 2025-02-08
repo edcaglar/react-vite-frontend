0e64cbf1bcef2955988b81ca99a17bbaa04b1b1a
edcaglar <edcaglar@gmail.com>
2025-02-08 22:53:41 +0300

    auth with cookie

---
+++ src/app/routes/landing.tsx
@@ -0,0 +1,28 @@
+import { useNavigate } from 'react-router';
+
+import { Button } from '@/components/ui/button';
+import { paths } from '@/config/paths';
+import { useUser } from '@/lib/auth';
+
+const LandingRoute = () => {
+  const navigate = useNavigate();
+  const user = useUser();
+
+  const handleStart = () => {
+    if (user.data) {
+      navigate(paths.app.dashboard.getHref());
+    } else {
+      navigate(paths.auth.login.getHref());
+    }
+    navigate(paths.auth.login.getHref());
+  };
+
+  return (
+    <div>
+      <h1>Welcome to bulletproof react</h1>
+      <Button onClick={handleStart}>Start</Button>
+    </div>
+  );
+};
+
+export default LandingRoute;
---
+++ src/config/paths.ts
@@ -0,0 +1,42 @@
+export const paths = {
+  home: {
+    path: '/',
+    getHref: () => '/',
+  },
+
+  auth: {
+    register: {
+      path: '/auth/register',
+      getHref: (redirectTo?: string | null | undefined) =>
+        `/auth/register${redirectTo ? `?redirectTo=${encodeURIComponent(redirectTo)}` : ''}`,
+    },
+    login: {
+      path: '/auth/login',
+      getHref: (redirectTo?: string | null | undefined) =>
+        `/auth/login${redirectTo ? `?redirectTo=${encodeURIComponent(redirectTo)}` : ''}`,
+    },
+  },
+
+  app: {
+    root: {
+      path: '/app',
+      getHref: () => '/app',
+    },
+    dashboard: {
+      path: '',
+      getHref: () => '/app',
+    },
+    discussions: {
+      path: 'tokens',
+      getHref: () => '/app/tokens',
+    },
+    token: {
+      path: 'discussions/:tokenId',
+      getHref: (id: string) => `/app/tokens/${id}`,
+    },
+    profile: {
+      path: 'profile',
+      getHref: () => '/app/profile',
+    },
+  },
+} as const;
